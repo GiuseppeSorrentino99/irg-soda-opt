875b2c2dd52c3dda15b3894c818ec8fdcc916cef
giuseppe.sorrentino <giuseppe.sorrentino@nags21.local.necst.it>
2025-04-28 22:31:37 +0200

    started modifications. Not working yet

--- docs/tutorials/tensorflow/docker-version/scripts/protobuf-to-tosa.sh
+++ docs/tutorials/tensorflow/docker-version/scripts/protobuf-to-tosa.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-
 set -e
 set -o pipefail
 
-docker run -u $(id -u):$(id -g) -v $(pwd):/working_dir --rm agostini01/soda \
-tf-mlir-translate \
-  --graphdef-to-mlir \
-  --tf-input-arrays=x1 \
-  --tf-input-data-types=DT_FLOAT \
-  --tf-input-shapes=4,32,32,1 \
-  --tf-output-arrays=Identity \
-  $1 \
-  -o output/tf.mlir
+INPUT_GRAPH=$1
+OUTPUT_TOSA=$2
 
 docker run -u $(id -u):$(id -g) -v $(pwd):/working_dir --rm agostini01/soda \
-tf-opt \
-  --tf-executor-to-functional-conversion \
-  --tf-region-control-flow-to-functional \
-  --tf-shape-inference \
-  --tf-to-tosa-pipeline \
-  output/tf.mlir \
-  -o $2
+  tf-mlir-translate \
+    --graphdef-to-mlir \
+    --tf-input-arrays=vxm_dense_source_input,vxm_dense_target_input \
+    --tf-input-data-types=DT_FLOAT,DT_FLOAT \
+    --tf-input-shapes="4,32,288,288,1;4,32,288,288,1 "\
+    --tf-output-arrays=Identity,Identity_1 \
+    "$INPUT_GRAPH" \
+    -o tf.mlir
+
+docker run -u $(id -u):$(id -g) -v $(pwd):/working_dir --rm agostini01/soda \
+  tf-opt \
+    --tf-executor-to-functional-conversion \
+    --tf-region-control-flow-to-functional \
+    --tf-shape-inference \
+    --tf-to-tosa-pipeline \
+    tf.mlir \
+    -o "$OUTPUT_TOSA"
