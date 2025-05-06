121b8e708c1445c27fd9d51e64637861b158cc58
giuseppe <gsorrentino@fpga-santa-vm-1.inside.dei.polimi.it>
2025-05-06 16:23:19 +0000

    restore tutorial

--- docs/tutorials/tensorflow/docker-version/scripts/protobuf-to-tosa.sh
+++ docs/tutorials/tensorflow/docker-version/scripts/protobuf-to-tosa.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
+
 set -e
 set -o pipefail
 
-INPUT_GRAPH=$1
-OUTPUT_TOSA=$2
-
 docker run -u $(id -u):$(id -g) -v $(pwd):/working_dir --rm agostini01/soda \
-  tf-mlir-translate \
-    --graphdef-to-mlir \
-    --tf-input-arrays=vxm_dense_source_input,vxm_dense_target_input \
-    --tf-input-data-types=DT_FLOAT,DT_FLOAT \
-    --tf-input-shapes="4,32,288,288,1;4,32,288,288,1" \
-    --tf-output-arrays=Identity,Identity_1 \
-    "$INPUT_GRAPH" \
+tf-mlir-translate \
+  --graphdef-to-mlir \
+  --tf-input-arrays=x1 \
+  --tf-input-data-types=DT_FLOAT \
+  --tf-input-shapes=4,32,32,1 \
+  --tf-output-arrays=Identity \
+  $1 \
   -o output/tf.mlir
 
 docker run -u $(id -u):$(id -g) -v $(pwd):/working_dir --rm agostini01/soda \
-  tf-opt \
-    --tf-executor-to-functional-conversion \
-    --tf-region-control-flow-to-functional \
-    --tf-shape-inference \
-    --tf-to-tosa-pipeline \
+tf-opt \
+  --tf-executor-to-functional-conversion \
+  --tf-region-control-flow-to-functional \
+  --tf-shape-inference \
+  --tf-to-tosa-pipeline \
   output/tf.mlir \
-    -o "$OUTPUT_TOSA"
+  -o $2
